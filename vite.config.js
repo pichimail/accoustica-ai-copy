686e2a71c2d92eaf138c786bc8cb6288c738a536
pichimail <pichimail24@gmail.com>
2026-01-10 11:51:34 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,16 +5,23 @@ import { dirname, resolve } from 'path'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
-// https://vite.dev/config/
 export default defineConfig({
-  logLevel: 'error', // Suppress warnings, only show errors
+  logLevel: 'error',
+
   plugins: [
     react(),
   ],
+
+  server: {
+    allowedHosts: [
+      'ta-01kek6py1asht8pb1tt7q627at-5173.wo-lp1kw27ez4o2l7ym3p8rou72t.w.modal.host'
+    ]
+  },
+
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src')
     },
     dedupe: ['react', 'react-dom']
   }
-});
+})
